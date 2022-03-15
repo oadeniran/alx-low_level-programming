6d87670e0d87b7367bfe35fb7cd2bd11a9a23abf
oadeniran <60529828+oadeniran@users.noreply.github.com>
2022-03-15 16:54:03 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,19 +5,19 @@
  *
  * Return: Always 0.
  */
-int print_alphabet(void)
-{
-	char alphs;
-
-	for (alphs = 'a'; alphs <= 'z'; alphs++)
-	{
-		_putchar(alphs);
-	}
-
-	_putchar('\n');
-	return (0);
-}
 int main()
 {
+	int print_alphabet(void)
+	{
+		char alphs;
+
+		for (alphs = 'a'; alphs <= 'z'; alphs++)
+		{
+			_putchar(alphs);
+		}
+
+		_putchar('\n');
+		return (0);
+	}
 	print_alphabet();
 }
